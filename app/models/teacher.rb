274ebdee038fc868c87c12be1c85e596e0a20400
Adjoa <adjoa.darien@gmail.com>
2018-03-23 18:55:19 +0000

    teacher model has validations

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,4 +3,8 @@ class Teacher < ApplicationRecord
     has_many :entries, through: :students
     has_many :events
     has_many :rehearsals, through: :events
+    
+    has_secure_password
+    validates :name, :username, :email, presence: true
+    validates :email, :email_format => { :message => 'does not look like a valid email address' }
 end
