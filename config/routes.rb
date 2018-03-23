b47a48ae4d740db00577189fb4bd8accc7d85b88
Adjoa <adjoa.darien@gmail.com>
2018-03-23 20:14:58 +0000

    users#welcome page displays

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'teachers#welcome'
-  get 'signin', to: 'sessions#new'
-  post 'signin', to: 'sessions#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
   
-  resources :teachers
+  resources :teachers, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
