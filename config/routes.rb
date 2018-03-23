3919aee936ad1588eff5cfb52dbdcf888dbae794
Adjoa <adjoa.darien@gmail.com>
2018-03-23 19:59:59 +0000

    init users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get '/signin', to: 'sessions/new'
-
-  post '/signin', to: 'sessions/create'
-
-  get '/logout', to: 'sessions/destroy'
-
+  root 'users#welcome'
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
