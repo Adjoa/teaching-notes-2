ffe58384148ba11cf7329f5a5fa8c609d7584fe3
Adjoa <adjoa.darien@gmail.com>
2018-03-24 13:46:45 +0000

    removed enclosing helpers block stmts from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  helpers do
-    def logged_in?
-      !!session[:teacher_id]
-    end
-    
-    def current_user
-      session[:teacher_id]
-    end
+  def logged_in?
+    !!session[:teacher_id]
+  end
+  
+  def current_user
+    session[:teacher_id]
   end
 
 end
