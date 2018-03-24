985710b63b2aa84a93a8a5092d60298df4e4f733
Adjoa <adjoa.darien@gmail.com>
2018-03-24 11:58:20 +0000

    added helpers #logged_in? and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  helpers do
+    def logged_in?
+      !!session[:teacher_id]
+    end
+    
+    def current_user
+      session[:name]
+    end
+  end
+
 end
