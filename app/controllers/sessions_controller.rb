9b8018b2a58375aff1bfb6459501b08f3c403e87
Adjoa <adjoa.darien@gmail.com>
2018-03-24 13:48:07 +0000

    fixed syntax errors in sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    @teacher = Teacher.new
   end
 
   def create
+    @teacher = Teacher.find_by(email: params[:email])
+    if @teacher.authenticate(params[:password])
+      current_user = @teacher.id
+      redirect_to teacher_path(@teacher)
+    else
+      render :new
+    end
   end
 
   def destroy
