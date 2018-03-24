4d1316428756f035cf4acea75c707623ab6917a5
Adjoa <adjoa.darien@gmail.com>
2018-03-24 03:25:15 +0000

    teacher resources are explicitly listed in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'teachers#welcome'
+  get 'signup', to: 'teachers#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
   
-  resources :teachers, except: [:index]
+  resources :teachers, only: [:create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
