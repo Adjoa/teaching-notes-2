f89ecbaa42217807ee7859c9b903d21236a2b5e8
Adjoa <adjoa.darien@gmail.com>
2018-03-26 16:10:33 +0000

    added student controller action stubs and students#index

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,29 @@ class StudentsController < ApplicationController
     user = User.find(current_user.id)
     @students = user.students
   end
+  
+  def show
+    @student = Student.find(params[:id])
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
 end
