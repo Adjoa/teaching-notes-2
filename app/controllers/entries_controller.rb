3f4325077c75179e29055c6caa7380139e99d6d5
Adjoa <adjoa.darien@gmail.com>
2018-03-27 16:10:43 +0000

    entries controller has actions for edit and update

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -21,10 +21,24 @@ class EntriesController < ApplicationController
     end
   end
   
+  def edit
+    @entry = Entry.find(params[:id])
+    @student = @entry.student
+  end 
+  
+  def update
+    @entry =Entry.find(params[:id])
+    if @entry.update(entry_params)
+      redirect_to student_entry_path(@entry.student, @entry), notice: "Entry was successfully updated."
+    else
+      render :edit
+    end
+  end 
+  
   def destroy
     @entry = Entry.find(params[:id])
     @entry.destroy
-    redirect_to student_path(@entry.student), notice: "#{@entry.title} was deleted."
+    redirect_to student_path(@entry.student), notice: "#{@entry.title} was destroyed."
   end
   
   private
