91ea953ea2f01d8c5feee36416982e6525806a28
Adjoa <adjoa.darien@gmail.com>
2018-03-27 03:45:58 +0000

    added entries#new entries#create and a private #entry_params method to controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,4 +6,25 @@ class EntriesController < ApplicationController
   def show
     @entry = Entry.find(params[:id])
   end
+  
+  def new
+    student = current_user.students.find(params[:student_id])
+    @entry = student.entries.build
+  end
+  
+  def create
+    student = current_user.students.find(params[:student_id])
+    @entry = student.entries.new(entry_params)
+    if @entry.save
+      redirect_to entries_path, notice: "Added new entry for #{student.name}." 
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def entry_params
+    params.require(:entry).permit(:title, :content)
+  end
 end
