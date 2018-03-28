427342d10f5b89ab302063d88f44697a7a0c7b64
Adjoa <adjoa.darien@gmail.com>
2018-03-28 20:48:58 +0000

    rehearsals#update checks for going field in params and udpates accordingly

--- app/controllers/rehearsals_controller.rb
+++ app/controllers/rehearsals_controller.rb
@@ -1,6 +1,7 @@
 class RehearsalsController < ApplicationController
   def index
     @rehearsals = current_user.rehearsals
+    @students = current_user.students
   end
   
   def new
@@ -19,6 +20,7 @@ class RehearsalsController < ApplicationController
   
   def show
     @rehearsal = Rehearsal.find(params[:id])
+    @students = current_user.students
   end
   
   def edit
@@ -26,9 +28,27 @@ class RehearsalsController < ApplicationController
   end 
   
   def update
+    # raise params.inspect
     @rehearsal = Rehearsal.find(params[:id])
-    @rehearsal.update(rehearsal_params)
-    redirect_to rehearsal_path(@rehearsal), notice: "Rehearsal for #{@rehearsal.event.name} was successfully updated."
+    student_id = params[:rehearsal][:student_id]
+    attending = params[:rehearsal][:going]
+    # attended = params[:rehearsal][:went]
+    
+    if student_id
+      attendance = Attendance.find_or_create_by(rehearsal_id: @rehearsal.id, student_id: student_id)
+      case 
+      when !params[:rehearsal][:going].nil?
+        attendance = attendance.update(going: attending)
+        binding.pry
+        redirect_to rehearsal_path(@rehearsal)
+      # when params[:rehearsal][:went]
+      #   attendance = attendance.update(went: attended)
+      #   redirect_to rehearsal_path(@rehearsal)
+      end
+    else
+      @rehearsal.update(rehearsal_params)
+      redirect_to rehearsal_path(@rehearsal), notice: "Rehearsal for #{@rehearsal.event.name} was successfully updated."
+    end
   end
   
   def destroy
@@ -40,6 +60,6 @@ class RehearsalsController < ApplicationController
   private
   
   def rehearsal_params
-    params.require(:rehearsal).permit(:event_id, :venue, :time)
+    params.require(:rehearsal).permit(:event_id, :venue, :time, :student_id, :going)
   end
 end
