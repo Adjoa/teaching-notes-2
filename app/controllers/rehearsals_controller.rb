683b94974444f12ce5bb2dadb8c2e7cc889649bd
Adjoa <adjoa.darien@gmail.com>
2018-03-28 00:08:34 +0000

    rehearsals controller has #new #create and #rehearsal_params methods

--- app/controllers/rehearsals_controller.rb
+++ app/controllers/rehearsals_controller.rb
@@ -2,4 +2,25 @@ class RehearsalsController < ApplicationController
   def index
     @rehearsals = Rehearsal.all
   end
+  
+  def new
+    @rehearsal = Rehearsal.new
+  end
+  
+  
+  def create
+    event = Event.find(params[:rehearsal][:event_id])
+    @rehearsal = event.rehearsals.build(rehearsal_params)
+    if @rehearsal.save
+      redirect_to rehearsals_path, notice: "Sucessfully created new rehearsal for #{event.name}"
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def rehearsal_params
+    params.require(:rehearsal).permit(:event_id, :venue, :time)
+  end
 end
