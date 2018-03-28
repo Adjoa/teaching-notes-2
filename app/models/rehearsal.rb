afd388e4fa60bd79de3b543ef263e9260dd1ed94
Adjoa <adjoa.darien@gmail.com>
2018-03-28 17:15:28 +0000

    added Rehearsal instance method #students_attending which uses Attendance scope method

--- app/models/rehearsal.rb
+++ app/models/rehearsal.rb
@@ -3,4 +3,10 @@ class Rehearsal < ApplicationRecord
   has_many :attendances
   has_many :students, through: :attendances
   delegate :user, :to => :event, :allow_nil => true
+  
+  def students_attending
+    self.attendances.going.collect do |attendance|
+      Student.find(attendance.student_id)
+    end
+  end
 end
