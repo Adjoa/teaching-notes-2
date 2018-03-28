bd9d8f3d6dd8ee9235c211132a63929a13e9811e
Adjoa <adjoa.darien@gmail.com>
2018-03-28 20:49:54 +0000

    added Student instance methods #rehearsals_attending and #count_rehearsals

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,14 @@ class Student < ApplicationRecord
   has_many :rehearsals, through: :attendances
   
   validates :name, :email, presence: true
+  
+  def rehearsals_attending
+    self.attendances.going.collect do |attendance|
+      Rehearsal.find(attendance.rehearsal_id)
+    end
+  end
+  
+  def count_rehearsals
+    self.rehearsals_attending.count
+  end
 end
