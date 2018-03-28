7269581cfb1d5e7a2d262dbcc761b7ae06bd32b7
Adjoa <adjoa.darien@gmail.com>
2018-03-28 03:24:51 +0000

    added event --#new #create #show actions to controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,12 +4,20 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = current_user.events.build
   end
 
   def create
+    @event = current_user.events.build(event_params)
+    if @event.save 
+      redirect_to event_path(@event), notice: "Event was successfully created."
+    else
+      render :new
+    end
   end
 
   def show
+    @event = current_user.events.find(params[:id])
   end
 
   def edit
